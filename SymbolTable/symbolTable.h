d565bd1142fabb695ee0aa2b4c256e30812c8d50
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2023-10-12 14:09:12 +0800

    Functional SymbolTable

--- SymbolTable/symbolTable.h
+++ SymbolTable/symbolTable.h
@@ -8,116 +8,38 @@
 
 #include "objectType.h"
 
+
+/**
+ * Symbol Table (Singleton)
+ * - Store the list of variables used in the program.
+**/
 class SymbolTable{
 
 // variables
 private:
+    // Store the list of variables
     std::map<std::string, ObjectType*> variableTable;
-    std::map<std::string, std::string> keywords = {
-        {"int", "int"},
-        {"float", "float"},
-        {"string", "string"},
-        {"bool", "bool"},
-        {"true", "bool"},
-        {"false", "bool"},
-        {"if", "if"},
-        {"else", "else"},
-        {"while", "while"},
-        {"for", "for"},
-        {"return", "return"},
-        {"print", "print"},
-        {"println", "println"},
-        {"read", "read"},
-        {"readln", "readln"},
-        {"and", "and"},
-        {"or", "or"},
-        {"not", "not"}
-    };
-    std::map<char, char> operators = {
-        {'+', '+'},
-        {'-', '-'},
-        {'*', '*'},
-        {'/', '/'},
-        {'%', '%'},
-        {'^', '^'},
-        {'>', '>'},
-        {'<', '<'},
-        {'=', '='},
-        {'!', '!'},
-        {':', ':'} // Assignment
-    };
-    std::map<char, char> alphabeth ={
-        {'a','a'},
-        {'b','b'},
-        {'c','c'},
-        {'d','d'},
-        {'e','e'},
-        {'f','f'},
-        {'g','g'},
-        {'h','h'},
-        {'i','i'},
-        {'j','j'},
-        {'k','k'},
-        {'l','l'},
-        {'m','m'},
-        {'n','n'},
-        {'o','o'},
-        {'p','p'},
-        {'q','q'},
-        {'r','r'},
-        {'s','s'},
-        {'t','t'},
-        {'u','u'},
-        {'v','v'},
-        {'w','w'},
-        {'x','x'},
-        {'y','y'},
-        {'z','z'},
-        {'A','A'},
-        {'B','B'},
-        {'C','C'},
-        {'D','D'},
-        {'E','E'},
-        {'F','F'},
-        {'G','G'},
-        {'H','H'},
-        {'I','I'},
-        {'J','J'},
-        {'K','K'},
-        {'L','L'},
-        {'M','M'},
-        {'N','N'},
-        {'O','O'},
-        {'P','P'},
-        {'Q','Q'},
-        {'R','R'},
-        {'S','S'},
-        {'T','T'},
-        {'U','U'},
-        {'V','V'},
-        {'W','W'},
-        {'X','X'},
-        {'Y','Y'},
-        {'Z','Z'},
-        {'_','_'},
-        {'0','0'},
-        {'1','1'},
-        {'2','2'},
-        {'3','3'},
-        {'4','4'},
-        {'5','5'},
-        {'6','6'},
-        {'7','7'},
-        {'8','8'},
-        {'9','9'}
-    };
-public:
+
+private:
     SymbolTable(){
         // Initialize the symbol table
         this->variableTable = std::map<std::string, ObjectType*>();
     }
 
-// Methods
+public:
+
+    // Delete the copy constructor and assignment operator
+    SymbolTable(SymbolTable const&) = delete;
+    void operator=(SymbolTable const&) = delete;
+
+public:
+    static SymbolTable& getInstance(){
+        static SymbolTable instance;
+        return instance;
+    }
+
+
+// non-destructive methods
 public:
     void addVariable(std::string name, ObjectType* variable){
         if(!this->isVariable(name)){
@@ -127,9 +49,11 @@ public:
         // ERROR: Variable already exists
         std::cout << "ERROR: Variable already exists\n";
     }
+
     ObjectType* getVariable(std::string name){
         return this->variableTable[name];
     }
+
     std::vector<std::string> getVariableNames(){
         std::vector<std::string> names;
         for(std::map<std::string, ObjectType*>::iterator it = this->variableTable.begin(); it != this->variableTable.end(); it++){
@@ -137,6 +61,7 @@ public:
         }
         return names;
     }
+
     std::vector<ObjectType*> getVariables(){
         std::vector<ObjectType*> variables;
         for(std::map<std::string, ObjectType*>::iterator it = this->variableTable.begin(); it != this->variableTable.end(); it++){
@@ -144,6 +69,57 @@ public:
         }
         return variables;
     }
+
+
+// Destructive Methods
+public:
+    
+    
+    // Assign the value of the variable
+    void assign(std::string name, ObjectType* variable){
+        
+        // Happens when you assign a variable that doesn't eixsts
+        if(!this->isVariable(name)){
+            // ERROR: Variable does not exist
+            std::cout << "ERROR: Variable does not exist\n";
+            return;
+        }
+        this->variableTable[name] = variable;
+    }
+
+    // Remove the variable from the symbol table
+    void remove(std::string name){
+        if(this->isVariable(name)){
+            this->variableTable.erase(name);
+            return;
+        }
+        // ERROR: Variable does not exist
+        std::cout << "ERROR: Variable does not exist\n";
+    }
+
+   
+// Parsing Auxillary Methods
+private:
+    ObjectTypeInt* parseToInt(ObjectType* variable){
+        if(variable->getType() == "integer"){
+            return (ObjectTypeInt*)variable;
+        }
+        // ERROR: Cannot convert to int
+        std::cout << "ERROR: Cannot convert to int\n";
+        return nullptr;
+    }
+
+    ObjectTypeDouble* parseToDouble(ObjectType *variable){
+        if(variable->getType() == "double"){
+            return (ObjectTypeDouble*)variable;
+        }
+        // ERROR: Cannot convert to double
+        std::cout << "ERROR: Cannot convert to double\n";
+        return nullptr;
+    }
+
+// Debug
+public:
     void printVariableTable(){
         std::cout << "Variable Table" << std::endl;
         for(std::map<std::string, ObjectType*>::iterator it = this->variableTable.begin(); it != this->variableTable.end(); it++){
@@ -159,17 +135,16 @@ public:
 
 // Checkers
 public:
-    bool isKeyword(std::string token){
-        return this->keywords.find(token) != this->keywords.end();
-    }
-    bool isOperator(char token){
-        return this->operators.find(token) != this->operators.end();
-    }
     bool isVariable(std::string token){
         return this->variableTable.find(token) != this->variableTable.end();
     }
-    bool isValidVariableCharacter(char token){
-        return this->alphabeth.find(token) != this->alphabeth.end();
-    }
 };
 #endif // SYMBOLTABLE_H
+
+
+/**
+ *
+ * Notes:
+ * 1. We don't need to support scope base on the instruction. Finish the instruction first before adding other shits.
+ *
+**/
